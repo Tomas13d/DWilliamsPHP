fe90ca7ac3786ae15c3c1046b2c29afbfe9255e9
Tomas13d <tomas.demobio@gmail.com>
2022-11-17 09:09:24 -0300

    wip: individualDev View

--- application/views/individualDev.php
+++ application/views/individualDev.php
@@ -2,8 +2,31 @@
     <div class="title-sub">
         <h5 class="subTitle-text"><strong>Villa Magdalena</strong></h5>
     </div>
-    <div>
-        
-    </div>
+    <div id="developments" class="carousel slide carrousel-margin" data-bs-ride="false">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="<?php echo base_url('public/assets/images/San_Luis1.jpg');?>"
+                            class="d-block w-100 dev-img-carousel" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?php echo base_url('public/assets/images/San_Luis1.jpg');?>"
+                            class="d-block w-100 dev-img-carousel" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?php echo base_url('public/assets/images/San_Luis1.jpg');?>"
+                            class="d-block w-100 dev-img-carousel" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="developments"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="developments"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
 
 </section>
